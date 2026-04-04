e9fc55a9116fbca48303c853bb8207531406fa2a
Yamparala Rahul <rahulvignanwork@gmail.com>
2026-04-04 14:42:23 +0530

    Use Wallpaper.png as app background

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,10 @@ import { Toaster } from "sonner";
 
 export default function App() {
   return (
-    <div className="min-h-dvh bg-slate-50">
+    <div
+      className="min-h-dvh bg-center bg-cover bg-no-repeat"
+      style={{ backgroundImage: "url('/images/Wallpaper.png')" }}
+    >
       <Resources />
       <Toaster position="top-right" richColors />
     </div>
